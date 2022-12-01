191b7802d522b355521fa366a76152e97db01120
Dipanshu <deepanshu.patil69@gmail.com>
2022-12-01 22:41:27 +0530

    brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./brand.css";
+import img1 from "../../assets/google.png";
+import img2 from "../../assets/slack.png";
+import img3 from "../../assets/shopify.png";
+import img4 from "../../assets/dropbox.png";
+import img5 from "../../assets/atlassian.png";
 
 const Brand = () => {
   return (
-    <div>
-      
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={img1} alt="google" />
+      </div>
+      <div>
+        <img src={img2} alt="slack" />
+      </div>
+      <div>
+        <img src={img5} alt="atlassian" />
+      </div>
+      <div>
+        <img src={img3} alt="shopify" />
+      </div>
+      <div>
+        <img src={img4} alt="dropbox" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
