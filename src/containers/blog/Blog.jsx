ef3988570c0ca0d4e681be9767607ad8343d9103
Dipanshu <deepanshu.patil69@gmail.com>
2022-12-04 03:08:34 +0530

    footer

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -8,7 +8,8 @@ const Blog = () => {
     <div className="gpt3__blog section__padding" id="blog">
       <div className="gpt3__blog-heading">
         <h1 className="gradient__text">
-          A lot is happening, We are blogging about it.
+          A lot is happening,
+          <br /> We are blogging about it.
         </h1>
       </div>
       <div className="gpt3__blog-container">
