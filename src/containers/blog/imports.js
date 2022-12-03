34602a7fcdd191d706d89f140a297d72c1f24fd2
Dipanshu <deepanshu.patil69@gmail.com>
2022-12-04 00:48:00 +0530

    blog

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,14 @@
+import blog01 from "../../assets/Rectangle22.png";
+import blog02 from "../../assets/Rectangle23.png";
+import blog03 from "../../assets/Rectangle24.png";
+import blog04 from "../../assets/Rectangle25.png";
+import blog05 from "../../assets/Rectangle26.png";
+
+
+export {
+    blog01,
+    blog02,
+    blog03,
+    blog04,
+    blog05
+}
